6bc155cd96294537e8bec9630777812009069717
Avilio Boscan <avilioabr@gmail.com>
2019-05-17 11:45:00 -0400

    compatibility with new cordova versions path files

--- hooks/after_plugin_install/001_account_manager_settings.js
+++ hooks/after_plugin_install/001_account_manager_settings.js
@@ -3,32 +3,48 @@
 
 var fs = require('fs');
 
-module.exports = function(context) {
+function directoryExists(path) {
+	try {
+		return fs.statSync(path).isDirectory();
+	} catch (e) {
+		return false;
+	}
+}
 
+module.exports = function (context) {
 	var cordova_util = context.requireCordovaModule('cordova-lib/src/cordova/util'),
-	ConfigParser = context.requireCordovaModule('cordova-lib/src/configparser/ConfigParser'),
-	projectRoot = cordova_util.isCordova(),
-	xml = cordova_util.projectConfig(projectRoot),
-	cfg = new ConfigParser(xml),
-	label = cfg.getPreference('AccountManagerLabel'),
-	iconUrl = cfg.getPreference('AccountManagerIconUrl'),
-	accountType = cfg.getPreference('AccountManagerType');
+		projectRoot = cordova_util.isCordova(),
+		xml = cordova_util.projectConfig(projectRoot),
+		ConfigParser,
+		newStructure = directoryExists('platforms/android/app/src/main/res/xml'),
+		basePath = newStructure ? 'platforms/android/app/src/main/res' : 'platforms/android/res';
+
+	try {
+		ConfigParser = context.requireCordovaModule('cordova-common/src/ConfigParser/ConfigParser');
+	} catch (e) {
+		ConfigParser = context.requireCordovaModule('cordova-lib/src/configparser/ConfigParser')
+	}
+
+	var cfg = new ConfigParser(xml),
+		label = cfg.getPreference('AccountManagerLabel'),
+		iconUrl = cfg.getPreference('AccountManagerIconUrl'),
+		accountType = cfg.getPreference('AccountManagerType');
 
 	fs.writeFileSync('platforms/android/res/drawable/acm_icon.png', fs.readFileSync(iconUrl));
 
-	var authenticatorFile = fs.readFileSync('platforms/android/res/xml/authenticator.xml','utf8');
+	var authenticatorFile = fs.readFileSync(basePath + '/xml/authenticator.xml', 'utf8');
 	authenticatorFile = authenticatorFile.replace(/android:icon="[ \S]*"/i, 'android:icon="@drawable/acm_icon"');
 	authenticatorFile = authenticatorFile.replace(/android:smallIcon="[ \S]*"/i, 'android:smallIcon="@drawable/acm_icon"');
-	authenticatorFile = authenticatorFile.replace(/android:accountType="[ \S]*"/i, 'android:accountType="'+accountType+'"');
-	fs.writeFileSync('platforms/android/res/xml/authenticator.xml', authenticatorFile);
+	authenticatorFile = authenticatorFile.replace(/android:accountType="[ \S]*"/i, 'android:accountType="' + accountType + '"');
+	fs.writeFileSync(basePath + '/xml/authenticator.xml', authenticatorFile);
 
-	var stringFile = fs.readFileSync('platforms/android/res/values/strings.xml','utf8');
-	if(stringFile.indexOf('<string name="authLabel">') > -1){
-		stringFile = stringFile.replace(/\<string name\=\"authLabel\"\>[ \S]*\<\/string\>/i, '<string name="authLabel">'+label+'</string>');
+	var stringFile = fs.readFileSync(basePath + '/values/strings.xml', 'utf8');
+	if (stringFile.indexOf('<string name="authLabel">') > -1) {
+		stringFile = stringFile.replace(/\<string name\=\"authLabel\"\>[ \S]*\<\/string\>/i, '<string name="authLabel">' + label + '</string>');
 	}
-	else{
-		stringFile = stringFile.replace('</resources>', '<string name="authLabel">'+label+'</string></resources>');
+	else {
+		stringFile = stringFile.replace('</resources>', '<string name="authLabel">' + label + '</string></resources>');
 	}
-	
-	fs.writeFileSync('platforms/android/res/values/strings.xml', stringFile);
+
+	fs.writeFileSync(basePath + '/values/strings.xml', stringFile);
 };
